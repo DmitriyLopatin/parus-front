bb7c13ed576d7316bbc241a337bf95f267f42fa3
DmitriyLopatin <97829709+DmitriyLopatin@users.noreply.github.com>
2022-12-02 10:32:12 +0600

    Update http.tsx

--- app/services/http.tsx
+++ app/services/http.tsx
@@ -1,13 +1,13 @@
 import axios from "axios";
 
- const BASE_URL = 'https://89.219.33.242/'
+ const BASE_URL = 'http://89.219.33.242/'
 
 
  const axiosInstance = axios.create({
     baseURL: `${BASE_URL}api/v1`,
     headers: {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
+//         'Access-Control-Allow-Origin': '*',
       },
  
  })
